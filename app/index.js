f4670e1eba7e3b7364db304862b0b3b9b40f0778
Caio Detz <89801456+kard-py@users.noreply.github.com>
2023-08-30 12:14:04 +0000

    TESTE

--- app/index.js
+++ app/index.js
@@ -1,5 +1,34 @@
-import { Text } from "react-native";
+import { useState, useEffect } from "react";
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 
-export default function Page() {
-  return <Text>Home page</Text>;
+import frases from "../assets/frases.json";
+
+export default function App() {
+  const [frase, setFrase] = useState("");
+  useEffect(() => {
+    setFrase(frases[Math.floor(Math.random() * frases.length)]);
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>{frase}</Text>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#8093F1",
+    padding: 8,
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "#fff",
+    fontStyle: "italic",
+    margin: 25,
+  },
+});
